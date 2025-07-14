0faa3d4a27ab5d8ca92db379d6a27ce0eb19f6cd
CHRISTUPREMCHANDNEELA <neelachristupremchand@gmail.com>
2025-07-14 23:29:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        return f"Thanks {name}, your feedback is submitted!"
     return render_template('form.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f"Thanks {name}, your feedback is submitted!"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
